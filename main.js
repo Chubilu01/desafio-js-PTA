013544566026f782cba770e484e16ecf3169ce78
Chubilu01 <5117.joaopedro@gmail.com>
2022-05-18 22:22:36 -0300

    just need some comments

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-const character = document.getElementsByClassName("character")[0];
-const containerCharacter = document.getElementsByClassName("container-character")[0];
+const character = document.getElementsByClassName("character")[0]; //selecionando a imagem do personagem pela classe
+const containerCharacter = document.getElementsByClassName("container-character")[0]; //selecionando a "div" do personagem pela classe
 
+const deathAudio = new Audio('./assets/deathsound.mp3'); //audio do personagem morrendo
+let isDead = false; //variável do estado do personagem (vivo/morto)
 
-const VELOCITY = 10;
+const VELOCITY = 10; //definindo qual distância o personagem deve percorrer por movimento
 
-const SCREEN_WIDTH = screen.width;
-const SCREEN_HEIGHT = screen.height;
+const SCREEN_WIDTH = screen.width; //definindo a variável que contém a largura da tela
+const SCREEN_HEIGHT = screen.height; //definindo a variável que contém a altura da tela
 
-let xPosition = 500;
-let yPosition = 300;
+let xPosition = 500; //definindo a posição inicial horizontal do personagem
+let yPosition = 300; //definindo a posição inicial vertical do personagem
 
-const keysAvaiable = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"]
-const directions = ["turnUp", "turnLeft", "turnRight", "turnDown"];
+const keysAvaiable = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"] //definindo as teclas do teclado que serão usadas para a movimentação
+const directions = ["turnUp", "turnLeft", "turnRight", "turnDown"]; //definindo os comandos que vão ser executados pelo personagem
 
+//
 window.addEventListener("keydown", (event) => {
     const key  = event.key;
 
@@ -26,27 +29,77 @@ window.addEventListener("keydown", (event) => {
         if(character.classList.contains(direction)) character.classList.remove(direction);
     })
 
+    if (!isDead) {
+        //definindo e limitando a movimentação vertical para cima do personagem (ao pressionar a "seta para cima")
+        if(key === "ArrowUp"){
+            character.classList.add("turnUp");
+            if (yPosition > 30) {
+                yPosition -= VELOCITY;
+            } else {
+                character.src = './assets/deadgif.gif';
+                character.style.height = '50px';
+                character.style.width = '50px';
+                deathAudio.play();
+                isDead = true;
+            }
+        }
 
-    if(key === "ArrowUp"){
-        character.classList.add("turnUp");
-        yPosition -= VELOCITY;
+        //definindo e limitando a movimentação vertical para baixo do personagem (ao pressionar a "seta para baixo")
+        if(key === "ArrowDown"){
+            character.classList.add("turnDown");
+            if (yPosition < (window.innerHeight - 90)) {
+                yPosition += VELOCITY;
+            } else {
+                character.src = './assets/deadgif.gif';
+                character.style.height = '50px';
+                character.style.width = '50px';
+                deathAudio.play();
+                isDead = true;
+                yPosition = window.innerHeight - 40;
+            }
+        }
+
+        //definindo e limitando a movimentação horizontal para esquerda do personagem (ao pressionar a "seta para esquerda")
+        if(key === "ArrowLeft"){
+            character.classList.add("turnLeft");
+            if (xPosition > -10) {
+                xPosition -= VELOCITY;
+            } else {
+                character.src = './assets/deadgif.gif';
+                character.style.height = '50px';
+                character.style.width = '50px';
+                deathAudio.play();
+                isDead = true;
+                xPosition = 10;
+            }
+        }
+
+        //definindo e limitando a movimentação horizontal para direita do personagem (ao pressionar a "seta para direita")
+        if(key === "ArrowRight"){
+            character.classList.add("turnRight");
+            if (xPosition < window.innerWidth - 110) {
+                xPosition += VELOCITY;
+            } else {
+                character.src = './assets/deadgif.gif';
+                character.style.height = '50px';
+                character.style.width = '50px';
+                deathAudio.play();
+                isDead = true;
+                xPosition = window.innerWidth - 60;
+            }
+        }
     }
 
-    if(key === "ArrowDown"){
-        character.classList.add("turnDown");
-        yPosition += VELOCITY;
-    }
-
-    if(key === "ArrowLeft"){
-        character.classList.add("turnLeft");
-        xPosition -= VELOCITY;
-    }
-
-    if(key === "ArrowRight"){
-        character.classList.add("turnRight");
-        xPosition += VELOCITY;
-    }
-
+    //garantindo que a "div do personagem" se movimente junto a ele (de acordo com a posição em pixels)
     containerCharacter.style.top = `${yPosition}px`;
-    containerCharacter.style.left = `${xPosition}px`
+    containerCharacter.style.left = `${xPosition}px`;
 });
+
+//adicionei essa parte para fazer o body ocupar 100% da tela e, após isso, coloquei uma borda vermelha delimitando o body
+const logo = document.getElementById('logo');
+logo.style.height = '68px';
+logo.style.width = '128px';
+document.body.style.height = '100vh';
+document.body.style.border = '3px solid red';
+
+alert('NÃO DEIXE ELE TOCAR NAS BORDAS, EU IMPLORO!');
